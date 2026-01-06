8c6c88b723d7c212dd08b7aeb9e045b3accdde7b
Alec McClure <blurmcclure16@gmail.com>
2026-01-06 10:08:37 -0600

    added nvidia powermanagement to home-desktop

--- hosts/home-desktop/nvidia.nix
+++ hosts/home-desktop/nvidia.nix
@@ -14,7 +14,7 @@
 	modesetting.enable = true;
 
 	# nvidia power managerment
-	powerManagement.enable = false;
+	powerManagement.enable = true;
 
 	# Fine-grained power management. Turns off GPU when not in use.
     	# Experimental and only works on modern Nvidia GPUs (Turing or newer).
