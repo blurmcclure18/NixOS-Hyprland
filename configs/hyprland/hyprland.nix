71fcaa380cac5ef5954297739e2dcf3f2dca8147
Alec McClure <blurmcclure16@gmail.com>
2026-03-31 23:35:52 -0500

    added device profile for MX Ergo mouse

--- configs/hyprland/hyprland.nix
+++ configs/hyprland/hyprland.nix
@@ -122,6 +122,12 @@
         };
       };
 
+      device = {
+        name = "logitech-mx-ergo-1";
+        sensitivity = 0.0;
+        accel_profile = "flat";
+      };
+
       cursor = {
 	no_hardware_cursors = true;
       };
