03dd7d0616016dcb59b1045e2e8a95707ada81f9
Alec McClure <blurmcclure16@gmail.com>
2026-05-05 15:51:32 -0500

    cleaned up home.nix file

--- home.nix
+++ home.nix
@@ -9,7 +9,6 @@
 
   imports = [
     ./configs
-    #inputs.zen-browser.homeModules.twilight
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -20,7 +19,7 @@
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
-  #
+
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
@@ -41,7 +40,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs;[
-    baobab
     calc
     cava
     chromium
@@ -59,37 +57,7 @@
     swappy
     tokyonight-gtk-theme
     usbutils
-    # # Adds the "hello" command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don"t forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command "my-hello" to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ""
-    #   echo "Hello, ${config.home.username}!"
-    # "")
   ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through "home.file".
-  home.file = {
-    # # Building this configuration will create a copy of "dotfiles/screenrc" in
-    # # the Nix store. Activating the configuration will then make "~/.screenrc" a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ""
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # "";
-  };
 
   home.sessionVariables = {
     HYPRLAND_CONFIG = "${config.home.homeDirectory}/.dotfiles/configs/hyprland/hyprland.conf";
@@ -99,20 +67,7 @@
 
   # Home Manager can also manage your environment variables through
   # "home.sessionVariables". These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don"t want to manage your shell
-  # through Home Manager then you have to manually source "hm-session-vars.sh"
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/alec/etc/profile.d/hm-session-vars.sh
-  #
+  # shell provided by Home Manager.
   home.sessionVariables = {
      EDITOR = "nvim";
   };
@@ -134,9 +89,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  
-	#  programs.zen-browser = {
-	#	enable = true;
-	#  };
 }
-
